1ef5b8ea666482874a51a7fd0d64887d5298f79f
kelompok1loss11-ctrl <kelompok1loss11@gmail.com>
2025-09-25 10:10:05 +0700

    Create main.py

---
+++ SRC /main.py
@@ -0,0 +1,6 @@
+Tujuan: Membuat Game Sejarah Indonesia Berbasis Construct 3
+Tools: Construct 3
++ #Rencana Proyek
++ Tujuan:Membuat Game Sejarah Indonesia Berbasis Construct 3 
++ Tools:Construct 3 
++ Fitur Utama:Kuis,Sistem Level 
